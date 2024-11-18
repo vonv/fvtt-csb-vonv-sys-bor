8f1ecc9b3110386f582c9b7f636a489b03fc9d89
vonv <archinormand@gmail.com>
2024-11-18 22:02:10 +0100

    hot reload

--- csb-vonv.js
+++ csb-vonv.js
@@ -10,7 +10,7 @@ async function welcomeMessage() {
   game.user.setFlag("vonv-fvtt-csb-everywhen", "welcomeMessageShown112024", true)
 }
   
-Hooks.on('ready', async function () {
+Hooks.on('hotReload', async function () {
   // mise à jour automatique des paramètres du système
   if (game.settings.get('custom-system-builder', 'customStyle')) {
     const settings = {
